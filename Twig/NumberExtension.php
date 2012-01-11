cd96d10725290e5ad4553367973cbbe84f706d61
lidaa <lidaa@localhost.lidaa>
2012-01-11 22:15:29 +0000

    Add NumberExtension

---
+++ Twig/NumberExtension.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Lidaa\TwigBundle\Twig;
+
+class NumberExtension extends \Twig_Extension {
+
+    public function getFunctions() {
+
+        $fonctions = array();
+
+        $fonctions['to_percent'] = new \Twig_Function_Method($this, 'toPercent');
+        $fonctions['to_readable_size'] = new \Twig_Function_Method($this, 'toReadableSize');
+
+        return $fonctions;
+    }
+
+    public function toPercent($number) {
+
+        $percent = number_format($number/100, 2);
+		  
+        if($number%100 == 0)
+           $percent = (int)$percent;	
+
+		  $percent = $percent . '%';
+
+        return $percent;
+    }
+
+    public function toReadableSize($number, $type = 'o') {
+
+		 switch(strtolower($type)) {
+		 	case 'k' : $number_readable = $number/1024; break;
+		 	case 'm' : $number_readable = $number/(1024*1024); break;
+		 	case 'g' : $number_readable = $number/(1024*1024*1024); break;
+		 	default : $number_readable = $number;
+		 } 
+        
+		  return $number_readable;
+    }
+
+    public function getName() {
+
+        return 'number';
+    }
+
+}

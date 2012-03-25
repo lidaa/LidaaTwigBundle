fdeef792783cfb09a5043483ede42acf9d7a47c0
Lidaa <aa_dil@hotmail.fr>
2012-03-25 14:25:51 +0000

    [TextExtension] new function ellipsize

--- Extension/TextExtension.php
+++ Extension/TextExtension.php
@@ -27,6 +27,7 @@ class TextExtension extends \Twig_Extension
         $fonctions = array();
 
         $fonctions['highlight'] = new \Twig_Function_Method($this, 'highlight', array('is_safe' => array('html')));
+        $fonctions['ellipsize'] = new \Twig_Function_Method($this, 'ellipsize', array('is_safe' => array('html')));
         
         return $fonctions;
     }
@@ -34,6 +35,11 @@ class TextExtension extends \Twig_Extension
     public function highlight($haystack, $string, $color = "#990000", $tag = 'span') 
     {
     	return $this->helper->highlight($haystack, $string, $color, $tag);    	
+    }
+
+    public function ellipsize($str, $max_length, $position = 1, $ellipsis = '&hellip;') 
+    {
+    	return $this->helper->ellipsize($str, $max_length, $position, $ellipsis) ;    	
     }
     
     public function getName()
--- Helper/TextHelper.php
+++ Helper/TextHelper.php
@@ -16,12 +16,36 @@ use Symfony\Component\Locale\Stub\StubNumberFormatter;
 class TextHelper
 {
     public function highlight($haystack, $string, $color, $tag)
-    {    	
+    {
         $highlight_str = sprintf('<%s style="color: %s">%s</%s>', $tag, $color, $string, $tag);
-    	
+
         $haystack = str_replace($string, $highlight_str, $haystack);
-    	
-    	return $haystack;
+
+        return $haystack;
     }
+
+    public function ellipsize($str, $max_length, $position, $ellipsis)
+    {
+        // Strip tags
+        $str = trim(strip_tags($str));
+
+        // Is the string long enough to ellipsize?
+        if (strlen($str) <= $max_length) {
+            return $str;
+        }
+
+        $beg = substr($str, 0, floor($max_length * $position));
+
+        $position = ($position > 1) ? 1 : $position;
+
+        if ($position === 1) {
+            $end = substr($str, 0, -($max_length - strlen($beg)));
+        } else {
+            $end = substr($str, -($max_length - strlen($beg)));
+        }
+
+        return $beg . $ellipsis . $end;
+    }
+
 }
 
